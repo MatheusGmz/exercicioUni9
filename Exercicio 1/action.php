fc2686a5ca8c8b7b52740522e0ccf38d8d6ceeaa
MatheusGmz <64435454+MatheusGmz@users.noreply.github.com>
2020-04-30 21:15:41 -0300

    Add files via upload

---
+++ Exercicio 1/action.php
@@ -0,0 +1,3 @@
+
+Olá, <?php echo $_GET["txtnome"];?>! </br>
+Você tem <?php echo $_GET["txtidade"]; ?> anos.
---
+++ Exercicio 1/form.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+     <form action="action.php" method = "GET">
+          <p>Nome: <input type="text" name="txtnome"/></p>
+          <p>Idade: <input type="text" name="txtidade"/></p>
+          <p><input type="submit" value="Enviar"/><p/>
+     </form>
+</body>
+</html>
