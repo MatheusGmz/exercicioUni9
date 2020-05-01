b6f5c4a5e3c01d168c09b947c0004593098abf1e
MatheusGmz <64435454+MatheusGmz@users.noreply.github.com>
2020-04-30 21:15:57 -0300

    Add files via upload

---
+++ Exercicio 2/form.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+     <form action="action.php" method = "POST">
+          <p>Nome: <input type="text" name="nome"/></p>
+          <p>Nota 1: <input type="text" name="nota1"/></p>
+          <p>Nota 2: <input type="text" name="nota2"/></p>
+          <p>Nota 3: <input type="text" name="nota3"/></p>
+          <p><input type="submit" value="Enviar"/><p/>
+     </form>
+</body>
+</html>
